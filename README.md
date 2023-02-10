8b8d5fedcea2bb57b324501b1d00dca6fab898bf
Vladislav Klyuchnikov <bllxy67@gmail.com>
2023-02-10 18:46:09 +0300

    edit readme.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# react-mesto-api-full
+
+Репозиторий для приложения проекта `Mesto`, включающий фронтенд и бэкенд части приложения со следующими возможностями: авторизации и регистрации пользователей, операции с карточками и пользователями. Бэкенд расположите в директории `backend/`, а фронтенд - в `frontend/`.
+
+https://github.com/Allxy/react-mesto-api-full
+
+`IP` 158.160.25.177
+`Frontend` https://qann1st.site
+`Backend` https://api.qann1st.site
--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -3,30 +3,30 @@ class Api {
     this._baseUrl = baseUrl;
     this._options = options;
 
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
     if (token) {
       if (!this._options.headers) this._options.headers = {};
-      this._options.headers.authorization = "Bearer " + token;
+      this._options.headers.authorization = 'Bearer ' + token;
     }
   }
 
   setToken(token) {
-    localStorage.setItem("token", token);
+    localStorage.setItem('token', token);
     if (!this._options.headers) this._options.headers = {};
-    this._options.headers.authorization = "Bearer " + token;
+    this._options.headers.authorization = 'Bearer ' + token;
   }
 
   removeToken() {
-    localStorage.removeItem("token");
+    localStorage.removeItem('token');
     if (this._options?.headers?.authorization) {
       delete this._options?.headers?.authorization;
     }
   }
 
-  async _fetch(path, method = "GET", body) {
+  async _fetch(path, method = 'GET', body) {
     const opt = { ...this._options, method };
     if (body)
-      if (typeof body === "string") opt.body = body;
+      if (typeof body === 'string') opt.body = body;
       else opt.body = JSON.stringify(body);
 
     const response = await fetch(this._baseUrl + path, opt);
@@ -38,51 +38,51 @@ class Api {
   }
 
   getUser() {
-    return this._fetch("/users/me");
+    return this._fetch('/users/me');
   }
 
   patchUser(data) {
-    return this._fetch("/users/me", "PATCH", data);
+    return this._fetch('/users/me', 'PATCH', data);
   }
 
   setAvatar(data) {
-    return this._fetch("/users/me/avatar", "PATCH", data);
+    return this._fetch('/users/me/avatar', 'PATCH', data);
   }
 
   addCard(data) {
-    return this._fetch("/cards", "POST", data);
+    return this._fetch('/cards', 'POST', data);
   }
 
   removeCard(id) {
-    return this._fetch("/cards/" + id, "DELETE");
+    return this._fetch('/cards/' + id, 'DELETE');
   }
 
   getCards() {
-    return this._fetch("/cards");
+    return this._fetch('/cards');
   }
 
   setLikeStatus(cardId, status) {
-    if (status) return this._fetch("/cards/" + cardId + "/likes", "PUT");
-    return this._fetch("/cards/" + cardId + "/likes", "DELETE");
+    if (status) return this._fetch('/cards/' + cardId + '/likes', 'PUT');
+    return this._fetch('/cards/' + cardId + '/likes', 'DELETE');
   }
 
   check() {
-    return this._fetch("/users/me", "GET");
+    return this._fetch('/users/me', 'GET');
   }
 
   login(data) {
-    return this._fetch("/signin", "POST", data);
+    return this._fetch('/signin', 'POST', data);
   }
 
   register(data) {
-    return this._fetch("/signup", "POST", data);
+    return this._fetch('/signup', 'POST', data);
   }
 }
 
-const baseUrl = process.env.REACT_APP_API_BASEURL ?? "http://localhost:4000";
+const baseUrl = process.env.REACT_APP_API_BASEURL ?? 'http://localhost:4000';
 export const mestoApi = new Api({
   baseUrl,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
